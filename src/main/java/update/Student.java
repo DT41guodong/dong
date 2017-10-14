832d3254287d7bc90c9fb8e076ebc7711b356c52
DT41guodong <13627265857@163.com>
2017-10-14 14:57:07 +0800

    ttt

--- src/main/java/update/Student.java
+++ src/main/java/update/Student.java
@@ -5,17 +5,20 @@
  * Date:2017年10月14日下午2:45:28
  * Copyright (c) 2017, bluemobi All Rights Reserved.
  *
-*/
+ */
 
 package update;
+
 /**
- * Description:	   <br/>
- * Date:     2017年10月14日 下午2:45:28 <br/>
- * @author   guoD
- * @version  
- * @see 	 
+ * Description: <br/>
+ * Date: 2017年10月14日 下午2:45:28 <br/>
+ * 
+ * @author guoD
+ * @version
+ * @see
  */
 public class Student {
-
+    public void show() {
+        System.out.println("1");
+    }
 }
-
